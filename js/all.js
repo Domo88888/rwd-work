731bc6b098993920030aea7622be8fb57e0cee9d
Domo <domoii0326@gmail.com>
2021-05-20 22:37:21 +0800

    update all pages

--- js/all.js
+++ js/all.js
@@ -4,4 +4,18 @@ $(document).ready(function(){
         $('body').toggleClass('menu-show');
         $(this).find('[data-fa-i2svg]').toggleClass('fa-bars').toggleClass('fa-chevron-down');
     });
+    $('.cart-inner-l ul li a').on('click', function(e){
+        e.preventDefault();
+        $(this).parent().siblings().find('a').removeClass('clicked');
+        $(this).toggleClass('clicked');
+    })
+    $('.fa-heart').on('click', function(e){
+        if($(this).attr('data-prefix') === 'far'){
+            $(this).attr('data-prefix', 'fas');
+            console.log("a");
+        }else if($(this).attr('data-prefix') === 'fas'){
+            $(this).attr('data-prefix', 'far');
+            console.log("XX");
+        }
+    })
 });
